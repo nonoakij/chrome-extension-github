39d64242f53e09d33318983f7079bc43d0ff8235
nonoakij <matsumoto_aki@cam-inc.co.jp>
2021-12-21 15:08:01 +0900

    support main branch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var element = document.createElement("button");
+const element = document.createElement("button");
 element.id = "chromeExtensionGithub-nonoakij";
 element.classList = "btn btn-sm branch";
 element.innerText = "マージコミットをコピーする";
 element.onclick = function() {
-  var text = [...document.querySelectorAll('[data-hovercard-type="pull_request"]')].reduce((all,c)=>{ var link = c.href.split('https://github.com/')[1]; return all ? `${all}<li>${link}</li>` : `<li>${link}</li>`},'');
+  const text = [...document.querySelectorAll('[data-hovercard-type="pull_request"]')].reduce((all,c)=>{ var link = c.href.split('https://github.com/')[1]; return all ? `${all}<li>${link}</li>` : `<li>${link}</li>`},'');
   window.navigator.clipboard.writeText(`<ul>${text}</ul>`).then(e => {
     alert('コピーしました');
   });
