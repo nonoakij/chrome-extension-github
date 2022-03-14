d87c8546a96a406be058b281992cc1ff88a6cba4
nonoakij <matsumoto_aki@cam-inc.co.jp>
2022-03-14 18:36:56 +0900

    typescript

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,31 @@ element.innerText = "マージコミットをコピーする";
 
 const comparePage = window.location.href.includes("/compare");
 
-var selector, target;
+let selector: string;
+let target: string;
 
 if (comparePage) {
-  element.classList = "btn btn-sm branch";
+  element.classList.add("btn","btn-sm","branch");
   selector = '[data-hovercard-type="pull_request"]';
   target = ".range-editor";
 } else {
-  element.classList =
-    "details-overlay details-reset position-relative js-reaction-popover-container js-comment-header-reaction-button d-none d-md-inline-block";
+  element.classList.add("btn","btn-sm","js-details-target","d-inline-block","float-left","float-none","m-0","mr-md-0","js-title-edit-button");
   selector =
     "[data-test-selector=pr-timeline-commits-list] [data-hovercard-type=pull_request]";
   target = ".timeline-comment-actions";
 }
 
 element.onclick = function () {
-  const text = [...document.querySelectorAll(selector)].reduce((all, c) => {
-    var link = c.href.split("https://github.com/")[1];
-    return all ? `${all}<li>${link}</li>` : `<li>${link}</li>`;
+  const text = [...document.querySelectorAll(selector)].reduce((p, c) => {
+    if (!(c instanceof HTMLAnchorElement)){
+      return p
+    }
+    const link = c.href.split("https://github.com/")[1];
+    return p.concat(`<li>${link}</li>`);
   }, "");
   window.navigator.clipboard.writeText(`<ul>${text}</ul>`).then(() => {
     alert("コピーしました");
   });
 };
 
-document.querySelector(target).prepend(element);
+window.document.querySelector(target)?.prepend(element);
